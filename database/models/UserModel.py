8fa38fcfcb42698b26c56a37b5fe36aba65ec364
31grudnia <mikolaj.starczewski@gmail.com>
2022-11-17 18:04:32 +0100

    0.1.6.1 - modded User with Columns for login to web app

--- database/models/UserModel.py
+++ database/models/UserModel.py
@@ -9,20 +9,43 @@ class UserSex(enum.Enum):
     male = 1
     female = 2
 
+class UserState(enum.Enum):
+    dolnośląskie = 1
+    kujawsko_pomorskie = 2
+    lubelskie = 3
+    lubuskie = 4
+    łódzkie = 5
+    małopolskie = 6
+    mazowieckie = 7
+    opolskie = 8
+    podkarpackie = 9
+    podlaskie = 10
+    pomorskie = 11
+    śląskie = 12
+    świętokrzyskie = 13
+    warmińsko_mazurskie = 14
+    wielkopolskie = 15
+    zachodniopomorskie = 16
+
+
 class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50))
     surname = Column(String(50))
-    city = Column(String(30), nullable=True)
+    city = Column(String(30))
+    state = Column(Enum(UserState))
+    street = Column(String(40))
+    home_number = Column(String(10))
+    post_code = Column(String(6))
     email = Column(String(50), unique=True)
     age = Column(Integer)
     phone_number = Column(String(15), unique=True)
     login = Column(String(50), unique=True)
     password = Column(String(80))
     photo = Column(String(50), nullable=True)
-    regulations = Column(Integer, nullable=True)
+    regulations = Column(Integer, nullable=True)        #???
     sex = Column(Enum(UserSex))
     hidden_posts = Column(Integer, nullable=True)
     coins = Column(Integer)
